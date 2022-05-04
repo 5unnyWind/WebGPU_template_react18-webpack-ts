811f2eb5ee05f2f2d2c98b725525a25610f147ff
sunnywind <sunny-wind@qq.com>
2022-05-04 12:35:12 +0800

    feat: hooks

---
+++ src/hooks/useDevice.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useDevice = () => {
+  const [adapter, setAdapter] = useState<GPUAdapter>();
+  const [device, setDevice] = useState<GPUDevice>();
+
+  useEffect(() => {
+    if (navigator.gpu === undefined) return;
+    const initWebGPU = async () => {
+      const adapter = await navigator.gpu.requestAdapter();
+      if (adapter === null) return;
+      const device = await adapter.requestDevice();
+      setAdapter(adapter);
+      setDevice(device);
+    };
+    initWebGPU();
+  }, []);
+  return { adapter, device };
+};
+
+export default useDevice;
